03c77d8610905a65b1a7585ff79f92cc388854a7
David Leonard <sephirothcloud1025@yahoo.com>
2014-06-03 23:27:33 -0400

    Adding task parser

--- todolist/app/models/Task.scala
+++ todolist/app/models/Task.scala
@@ -4,6 +4,15 @@ case class Task(id: Long, label: String)
 
 object Task {
 
+    // Task is a parser that, given a JDBC `ResultSet` row with at least 
+    // an id and a label column, is able to create a `Task` value.
+    
+    val task = {
+        get[Long]("id") ~
+        get[String]("label") map {
+            case id-label => Task(id, label)
+        }
+    }
     def all(): List[Task] = Nil
 
     def create(label: String) {}
