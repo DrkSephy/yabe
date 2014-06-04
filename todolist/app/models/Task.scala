4255f4f91f4f85fbcdcfb186543714133cb57472
David Leonard <sephirothcloud1025@yahoo.com>
2014-06-04 18:13:01 -0400

    Updating tasks

--- todolist/app/models/Task.scala
+++ todolist/app/models/Task.scala
@@ -6,14 +6,15 @@ object Task {
 
     // Task is a parser that, given a JDBC `ResultSet` row with at least 
     // an id and a label column, is able to create a `Task` value.
-    
+
     val task = {
         get[Long]("id") ~
         get[String]("label") map {
             case id-label => Task(id, label)
         }
     }
-    def all(): List[Task] = Nil
+    def all(): List[Task] = DB.withConnection { implicit c => 
+        SQL("select * from task").as(task *)}
 
     def create(label: String) {}
 
