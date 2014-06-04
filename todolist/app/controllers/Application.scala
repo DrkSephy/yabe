e6f527fcfd712cb22677d0c0b0da9e86e8611388
David Leonard <sephirothcloud1025@yahoo.com>
2014-06-03 23:06:33 -0400

    Adding template for forms

--- todolist/app/controllers/Application.scala
+++ todolist/app/controllers/Application.scala
@@ -5,8 +5,10 @@ import play.api.mvc._
 
 object Application extends Controller {
 
+  // Use redirect instead of `OK` to specify a 303 See Other HTTP Response type.
+  // We use the reverse router to get the URL needed to fetch the `tasks` actions.
   def index = Action {
-    Ok("Hello World")
+    Redirect(routes.Application.tasks)
   }
 
   // TODO is a built-in action that returns a 501 Not Implemented HTTP Response
